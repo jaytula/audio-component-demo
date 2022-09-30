e86b9859f7ced5c210899130bf24b66f9c7bdf60
Jay T <jaytula@gmail.com>
2022-09-30 14:28:24 -0700

    Implement AudioPlayer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AudioPlayer from "./AudioPlayer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AudioPlayer src="/assets/spirit-blossom-15285.mp3" />
     </div>
   );
 }
